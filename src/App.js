3e400625acdced3d774b3563a377903671dc6778
Bunny Mueller <thebunnymueller@gmail.com>
2022-04-02 17:16:23 +0200

    Pokemon API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then(response => {
+          return response.json();
+      }).then(response => {
+          setPokemonList(response.results);
+      }).catch(error=>{
+          console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch Pokemon:</h1>
+      {pokemonList.map((pokemonDictionary, index) => {
+        return (<p key={index}>{pokemonDictionary.name}</p>);
+      })}
     </div>
   );
 }
 
 export default App;
+
